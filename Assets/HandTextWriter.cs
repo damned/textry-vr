45856243632ce4f285db08a04f4904a1a7dc86c6
damned <writetodan@yahoo.com>
2017-11-19 10:56:09 +0000

    Basic leap (linux, v2.3) and letters

---
+++ Assets/HandTextWriter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Leap;
+
+public class HandTextWriter : MonoBehaviour {
+	
+	public Text displayText;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		ResetDisplay();
+		var controller = GetComponent<HandController>();
+		var frame = controller.GetFrame();
+		var points = frame.Pointables;
+		Debug("points count: " + points.Count);
+		Debug("hands: " + HandsInfo(frame.Hands));
+	}
+
+  private string HandsInfo(HandList hands)
+	{
+		return "l: " + HandInfo(hands.Leftmost) + "; r: " + HandInfo(hands.Rightmost);
+	}
+
+  private string HandInfo(Hand hand)
+	{
+		return string.Format("grab: {0:F1}, pinch: {0:F1}", hand.GrabStrength, hand.PinchStrength);
+	}
+
+	private void Debug(string message)
+	{
+		displayText.text += message + "\n";
+	}
+
+	private void ResetDisplay()
+	{
+		displayText.text = "";
+	}
+}
