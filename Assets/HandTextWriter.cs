4d43a5f8e4e1a298b95198bc167a7d7f7a95922b
damned <writetodan@yahoo.com>
2017-11-19 20:37:22 +0000

    Proportionate grab only one

--- Assets/HandTextWriter.cs
+++ Assets/HandTextWriter.cs
@@ -14,6 +14,7 @@ public class HandTextWriter : MonoBehaviour {
 	public float range = 2f;
 
 	private HandController controller;
+	private Collider lastGrabbed = null;
 
 	void Start () {
 		controller = GetComponent<HandController>();
@@ -49,40 +50,53 @@ public class HandTextWriter : MonoBehaviour {
 		Collider[] close_things = Physics.OverlapSphere(grabPosition, range);
     Vector3 distance = new Vector3(1, 0.0f, 0.0f);
 
-		Collider grabbed_ = null;
+		Collider grabbed = null;
 
     for (int j = 0; j < close_things.Length; ++j) {
       Vector3 new_distance = grabPosition - close_things[j].transform.position;
       if (close_things[j].GetComponent<Rigidbody>() != null && new_distance.magnitude < distance.magnitude &&
           !close_things[j].transform.IsChildOf(transform)) {
-        grabbed_ = close_things[j];
+        grabbed = close_things[j];
         distance = new_distance;
       }
     }
-    Debug("Grabbed: " + grabbed_);
-		if (grabbed_ == null)
+    Debug("Grabbed: " + grabbed);
+		if (lastGrabbed != null && lastGrabbed != grabbed)
+		{
+			ChangeColour(lastGrabbed, Color.white);	
+			lastGrabbed = null;
+		} 
+		if (grabbed == null)
 		{
 	    Debug("Nearly grabbed things: " + string.Join(", ", close_things.ToList().Select(t => t.gameObject.name).ToArray()));
 			return;
 		}
-    Debug("Grabbed: " + grabbed_.gameObject.name);
-		Material material = grabbed_.gameObject.GetComponent<Renderer>().material;
+		lastGrabbed = grabbed;
+    Debug("Grabbed: " + grabbed.gameObject.name);
+		Material material = grabbed.gameObject.GetComponent<Renderer>().material;
 		if (hand.GrabStrength > 0.7) {
-			material.color = Color.red;
+			ChangeColour(grabbed, Color.red);
 		}
 		else if (hand.GrabStrength < 0.2) {
-			material.color = Color.black;
+			ChangeColour(grabbed, Color.black);
 		}
+	}
+
+	private void ChangeColour(Collider collider, Color color)
+	{
+		Material material = collider.gameObject.GetComponent<Renderer>().material;
+		material.color = color;
 	}
 
 	private void PlaceLetters(GameObject letters, GameObject interactables)
 	{
-		var spacing = 0.12f;
-		var slots = 5;
+		var spacing = 0.1f;
+		var yOffset = 1f;
+		var slots = 6;
 		var index = 0;
 		var start = -(spacing * slots) / 2;
 		var x = start;
-		var y = start + spacing;
+		var y = start + yOffset;
 		var xIndex = 0;
 		var z = 0f;
 
@@ -94,7 +108,7 @@ public class HandTextWriter : MonoBehaviour {
 				xIndex = index % slots; 
 				if (xIndex == 0)
 				{ 
-					y += spacing;
+					y -= spacing;
 					x = start;
 				}
 				else
