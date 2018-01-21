3375090408c509f4ae73bf13794e68e28a5c3194
tw-manc <dmoore@thoughtworks.com>
2018-01-21 14:15:06 +0000

    Basic reach for word, cannot change mind though

--- Assets/Scripts/GesturesStrategy.cs
+++ Assets/Scripts/GesturesStrategy.cs
@@ -4,89 +4,99 @@ using UnityEngine;
 
 public class GesturesStrategy
 {
-  private readonly Knobs knobs;
-  private readonly IDebug debug;
-  private readonly KnobArranger knobArranger;
-  
-  private int layer = 0;
-  private string text = ""; 
+    private readonly Knobs knobs;
+    private readonly IDebug debug;
+    private readonly KnobArranger knobArranger;
 
-  private readonly Gestures gestures;
-  private readonly List<string> words = new List<string>();
+    private int layer = 0;
+    private string text = "";
 
-  public GesturesStrategy(Gestures gestures, KnobArranger knobArranger, IDebug debug)
-  {
-    this.knobArranger = knobArranger;
-    this.debug = debug;
-    this.gestures = gestures;
+    private readonly Gestures gestures;
+    private readonly List<string> words = new List<string>();
 
-    // move up and out to text writer, use lister interface
-    gestures.GestureFor(HandSide.Right).OnGrab += OnGrab;
-    gestures.GestureFor(HandSide.Left).OnGrab += OnGrab;
-    gestures.GestureFor(HandSide.Right).OnRelease += OnRelease;
-    gestures.GestureFor(HandSide.Left).OnRelease += OnRelease;
-    gestures.GestureFor(HandSide.Right).OnTouch += OnTouch;
-    gestures.GestureFor(HandSide.Left).OnTouch += OnTouch;
-  }
+    public GesturesStrategy(Gestures gestures, KnobArranger knobArranger, IDebug debug)
+    {
+        this.knobArranger = knobArranger;
+        this.debug = debug;
+        this.gestures = gestures;
 
-  public string OnHandUpdate(IHand hand)
-  {
-    // move up and out to text writer
-    var side = hand.Side();
-    var gesture = gestures.GestureFor(side);
-    
-
-    // debug.Log("hand side: " + hand.Side());
-
-    // yes this is a bit odd, around construction-time access to vrtk
-    // instances - have a look sometime when on vive setup 
-    gesture.hand = hand;
-
-    gesture.OnHandUpdate(hand);
-    debug.Log("text: " + text + ", words: " + String.Join(", ", words.ToArray()));
-    return text;
-  }
-
-  public bool IsGrabbing(HandSide side)
-  {
-    return gestures.GestureFor(side).IsGrabbing;
-  }
-
-  public void OnGrab(Gesture gesture, Knob knob)
-  {
-    layer += 1;
-    text += knob.Text();
-    string arrangement = knobArranger.Arrange(layer * 0.08f, text);
-    debug.Log(arrangement);
-  }
-
-  public void OnTouch(Gesture gesture, Knob knob)
-  {
-    debug.Log($"touch: knob layer {knob.Layer}, layer {layer}");
-  }
-
-  public void OnRelease(Knob knob)
-  {
-    if (!gestures.AnyGrabs()) {
-      CompleteWord();
+        // move up and out to text writer, use lister interface
+        gestures.GestureFor(HandSide.Right).OnGrab += OnGrab;
+        gestures.GestureFor(HandSide.Left).OnGrab += OnGrab;
+        gestures.GestureFor(HandSide.Right).OnRelease += OnRelease;
+        gestures.GestureFor(HandSide.Left).OnRelease += OnRelease;
+        gestures.GestureFor(HandSide.Right).OnTouch += OnTouch;
+        gestures.GestureFor(HandSide.Left).OnTouch += OnTouch;
     }
-  }
 
-  private void CompleteWord()
-  {
-    words.Add(text);
-    text = "";
-    layer = 0;
-    knobArranger.ResetLayers();
-  }
+    public string OnHandUpdate(IHand hand)
+    {
+        // move up and out to text writer
+        var side = hand.Side();
+        var gesture = gestures.GestureFor(side);
 
-  public string Text()
-  {
-    return text;
-  }
 
-  public List<string> Words()
-  {
-    return words;
-  }
+        // debug.Log("hand side: " + hand.Side());
+
+        // yes this is a bit odd, around construction-time access to vrtk
+        // instances - have a look sometime when on vive setup 
+        gesture.hand = hand;
+
+        gesture.OnHandUpdate(hand);
+        debug.Log("text: " + text + ", words: " + String.Join(", ", words.ToArray()));
+        return text;
+    }
+
+    public bool IsGrabbing(HandSide side)
+    {
+        return gestures.GestureFor(side).IsGrabbing;
+    }
+
+    public void OnGrab(Gesture gesture, Knob knob)
+    {
+        AddLayer(knob);
+    }
+
+    private void AddLayer(Knob knob)
+    {
+        layer += 1;
+        text += knob.Text();
+        string arrangement = knobArranger.Arrange(layer * 0.08f, text);
+        debug.Log(arrangement);
+    }
+
+    public void OnTouch(Gesture gesture, Knob knob)
+    {
+        debug.Log($"touch: knob layer {knob.Layer}, layer {layer}");
+        if (knob.Layer == layer && gestures.AnyGrabs())
+        {
+            AddLayer(knob);
+        }
+    }
+
+    public void OnRelease(Knob knob)
+    {
+        if (!gestures.AnyGrabs())
+        {
+            CompleteWord();
+        }
+    }
+
+    private void CompleteWord()
+    {
+        words.Add(text);
+        text = "";
+        layer = 0;
+        knobArranger.ResetLayers();
+    }
+
+    public string Text()
+    {
+        return text;
+    }
+
+    public List<string> Words()
+    {
+        return words;
+    }
 }
